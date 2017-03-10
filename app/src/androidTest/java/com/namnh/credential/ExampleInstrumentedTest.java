d8c777d5bb5b3d3890ebbccc06337fd571cced63
huunam118 <huunam118@gmail.com>
2017-03-10 10:14:11 +0700

    Add UnitTest

--- app/src/androidTest/java/com/namnh/credential/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/namnh/credential/ExampleInstrumentedTest.java
@@ -2,12 +2,28 @@ package com.namnh.credential;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
+import android.view.View;
+import android.widget.TextView;
+import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +32,57 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private MainActivity mActivity = null;
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setActivity() {
+        mActivity = mActivityRule.getActivity();
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.namnh.credential", appContext.getPackageName());
+    }
+
+    @Test
+    public void processInput() {
+        onView(withId(R.id.edt_plaintext)).perform(typeText("password"),
+                closeSoftKeyboard());
+
+        onView(withId(R.id.btn_start)).perform(click());
+        String encryptedText = getText(withId(R.id.tv_encrypted));
+        assertNotEquals("password", encryptedText);
+        onView(withId(R.id.tv_decrypted)).check(matches(withText("password")));
+    }
+
+    /**
+     * Try to getText from TextView
+     */
+    private String getText(final Matcher<View> matcher) {
+        final String[] stringHolder = { null };
+        onView(matcher).perform(new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(TextView.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "getting text from a TextView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                TextView tv = (TextView) view;
+                stringHolder[0] = tv.getText().toString();
+            }
+        });
+        return stringHolder[0];
     }
 }
